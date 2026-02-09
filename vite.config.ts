41f9eab15e987067ba37c69c2fb1bc94d89bd364
kevindbannister <kevin@kevindbannister.co.uk>
2026-02-09 20:50:38 +0000

    Fix menu icons by using lucide-react

--- vite.config.ts
+++ vite.config.ts
@@ -14,7 +14,6 @@ export default defineConfig({
   },
   resolve: {
     alias: {
-      'lucide-react': resolveShim('./src/shims/lucide-react.tsx'),
       'react-router-dom': resolveShim('./src/shims/react-router-dom.tsx')
     }
   }
