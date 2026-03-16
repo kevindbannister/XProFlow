529ff9345978117c1babfd0ef5430cde6b18832d
Kevin Bannister <kevinbannister@Kevins-MacBook-Pro.local>
2026-03-16 20:10:34 +0000

    Implement SaaS layout system

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -17,7 +17,7 @@ export default {
         }
       },
       boxShadow: {
-        page: '0 0 0 1px rgba(18,29,49,0.03), 0 8px 24px rgba(18,29,49,0.06)',
+        page: '-10px 0 24px rgba(18,29,49,0.12), 0 0 0 1px rgba(18,29,49,0.04)',
         card: '0 1px 2px rgba(18,29,49,0.04)'
       }
     }
